862386b37a0ecb4bb39ee97c56081a66ad3d7dcf
fleekgonzalo <114927947+fleekgonzalo@users.noreply.github.com>
2023-06-08 10:28:57 -0300

    Update Footer.jsx

--- components/Layout/Footer.jsx
+++ components/Layout/Footer.jsx
@@ -159,7 +159,7 @@ export const getSocials = () => (
           key={`social-${social.type}`}
           aria-label={`social-${social.type}`}
         >
-          <Image src={src} alt="" width={18} height={16} />
+          <img src={src} alt="" width={18} height={16} />
         </a>
       );
     })}
